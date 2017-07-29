ed6c804cc9f6902829e46ea8f1438efeab38cac3
DiepEsc <TranTienTung94@gmail.com>
2017-07-29 15:12:29 +0700

    Update NhacVuiDownAll.user.js

--- NhacVuiDownAll.user.js
+++ NhacVuiDownAll.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Điệp Esc
 // @include      /http:\/\/nhac\.vui\.vn/[^.]*\-a[a-z0-9]*.html/
 // @grant        none
 // ==/UserScript==
